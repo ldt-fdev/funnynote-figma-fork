db22b596c5c3b6c8597a621360626746162307db
luongductrong <luongductrong2004@gmail.com>
2025-07-22 17:18:48 +0700

    Create: Add Prettier config, UI components, and update dependencies
    Added .prettierrc and VS Code settings for consistent code formatting. Introduced new UI components: Resizable and Toggle. Updated next.config.ts to include custom headers and allowedDevOrigins. Enhanced Tailwind CSS setup with typography plugin and Tiptap placeholder styles. Updated dependencies in package.json to include @radix-ui/react-toggle, @tailwindcss/typography, @tiptap/extension-placeholder, and react-resizable-panels.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/extension/iframe', // hoặc "/(.*)" nếu toàn bộ site được nhúng
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value:
+              'frame-ancestors https://note.funnycode.dev https://note.funnycode.vn https://funnycode.vn http://ftes.cloud http://localhost:3000', // hoặc '*', nếu bạn chấp nhận tất cả
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL', // Hoặc bỏ nếu đã dùng CSP
+          },
+        ],
+      },
+    ];
+  },
+  allowedDevOrigins: [
+    'https://note.funnycode.dev',
+    'https://note.funnycode.vn',
+    'https://funnycode.vn',
+    'http://ftes.cloud',
+    'http://localhost:3000',
+    '*',
+  ],
 };
 
 export default nextConfig;
